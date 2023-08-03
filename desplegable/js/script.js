261f39b3c0c6b9e4451384a094cf7a740660157a
joseRamirezgomez <joseramirezgomez00@gmail.com>
2023-08-03 06:32:21 -0500

    ejemplo ejercicio desplegable

---
+++ desplegable/js/script.js
@@ -0,0 +1,28 @@
+const icon = document.getElementById("icon");
+const select = document.querySelector(".seleccion");
+let menuVisible = false;
+
+function mostrarOcultarMenu() {
+
+    if (menuVisible) {
+       
+        icon.classList.remove("fa-x");
+        icon.classList.add("fa-bars");
+        select.style.transform = "translateX(2px)";
+        menuVisible = false;
+    } else {
+       
+        icon.classList.remove("fa-bars");
+        icon.classList.add("fa-x");
+        select.style.transform = "translateX(130px)";
+        menuVisible = true;
+    }
+}
+
+function seleccionar() {
+ 
+    icon.classList.remove("fa-x");
+    icon.classList.add("fa-bars");
+    select.style.transform = "translateX(2px)";
+    menuVisible = false;
+}
