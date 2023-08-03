e4e259f7a0df5e32cc1d5b5ad6a9f861012c719e
joseRamirezgomez <joseramirezgomez00@gmail.com>
2023-08-03 06:54:57 -0500

    correccion del js en la carpeta desplegable

--- desplegable/js/script.js
+++ desplegable/js/script.js
@@ -5,13 +5,13 @@ let menuVisible = false;
 function mostrarOcultarMenu() {
 
     if (menuVisible) {
-       
+        document.getElementById("nav").classList = "";
         icon.classList.remove("fa-x");
         icon.classList.add("fa-bars");
         select.style.transform = "translateX(2px)";
         menuVisible = false;
     } else {
-       
+        document.getElementById("nav").classList = "responsive"
         icon.classList.remove("fa-bars");
         icon.classList.add("fa-x");
         select.style.transform = "translateX(130px)";
@@ -20,7 +20,7 @@ function mostrarOcultarMenu() {
 }
 
 function seleccionar() {
- 
+    document.getElementById("nav").classList = "";
     icon.classList.remove("fa-x");
     icon.classList.add("fa-bars");
     select.style.transform = "translateX(2px)";
